5221e0d7b5059b7af74b7cbf6a763ac071443ab0
ankitmehta94 <ankitmehta94@gmail.com>
2015-10-07 17:43:40 +0530

    Create todos.js

---
+++ js/collections/todos.js
@@ -0,0 +1,26 @@
+// js/collections/todos.js
+var app=app||{};
+var TodoList=Backbone.Collection.extend({
+	model:app.Todo,
+	localStorage: new Backbone.LocalStorage('todo-backbone'),
+	completed : function(){
+		return this.filter(function(todo){
+			return todo.get('completed');
+		});
+	},
+	remaining : function(){
+		return this.without.apply(this,this.completed());
+	},
+	nextOrder : function(){
+		if(!this.length){
+			return 1;
+			}
+			return this.last().get('order')+1;
+		},
+	comparator : function(todo){
+		return todo.get('order');
+		},
+	  
+	
+	});
+	app.Todos= new TodoList();
